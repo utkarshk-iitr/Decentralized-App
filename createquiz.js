a3cff404a1f0918eec6c23a3641e2dc271b7c330
utkarshk-iitr <utkarsh_k@cs.iitr.ac.in>
2025-08-03 23:23:23 +0530

    js files created

--- createquiz.js
+++ createquiz.js
@@ -40,3 +40,4 @@ function createquiz(){
     });
 }
 export default createquiz;
+
--- db.js
+++ db.js
@@ -3,6 +3,7 @@ import express from "express";
 import dotenv from "dotenv";
 dotenv.config()
 
+
 const app = express();
 const connectdb = async () => {
    
